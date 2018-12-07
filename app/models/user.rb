4ecdcd166948aac42fea6752a6ac8f973a89049e
Sebastian Torres <sebastiantyz@gmail.com>
2018-12-07 14:53:55 -0500

    Fix rubocop error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates_presence_of :first_name, :last_name
+  validates :first_name, :last_name, presence: true
 end
