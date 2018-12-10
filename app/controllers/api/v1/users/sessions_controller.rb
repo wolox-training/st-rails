01b51096bf9901c6f4eddd875e0be4d90eb5b1d8
Sebastian Torres <sebastiantyz@gmail.com>
2018-12-10 09:25:09 -0500

    Rubocop offenses fixed

--- app/controllers/api/v1/users/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::Users::SessionsController < ApplicationController
-
-  def create
-    puts 'hablame!'
-    render json: {msg: 'hola'}
-  end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
-  
+
   validates :first_name, :last_name, presence: true
 end
