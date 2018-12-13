1438cda4bbdb9f103bc77db64aa2b500f637544a
Sebastian Torres <sebastiantyz@gmail.com>
2018-12-13 18:20:38 -0500

    Validate locale in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  LOCALES = ['es', 'en']
+
   devise :database_authenticatable, :registerable, :validatable
 
   include DeviseTokenAuth::Concerns::User
 
   has_many :rents, dependent: :nullify
 
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :locale, presence: true
+  validates_inclusion_of :locale, in: LOCALES
 end
