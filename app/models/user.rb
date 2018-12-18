cab0289b106d256e36c8bc835992d595d7b14de0
Sebastian Torres <sebastiantyz@gmail.com>
2018-12-18 14:08:13 -0500

    Added has many book suggestion relation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
 
   has_many :rents, dependent: :nullify
+  has_many :book_suggestions, dependent: :nullify
 
   validates :first_name, :last_name, :locale, presence: true
   validates :locale, inclusion: LOCALES
