ce0873c76e5b7ca56a73d557867207f57a7f3a4e
Sebastian Torres <sebastiantyz@gmail.com>
2018-12-19 10:34:36 -0500

    Rubocop offense fixed

--- app/serializers/book_suggestion_serializer.rb
+++ app/serializers/book_suggestion_serializer.rb
@@ -1,4 +1,5 @@
 class BookSuggestionSerializer < ActiveModel::Serializer
-  attributes :id, :author, :title, :link, :editor, :year, :price, :synopsis, :created_at, :updated_at
+  attributes :id, :author, :title, :link, :editor, :year, :price,
+             :synopsis, :created_at, :updated_at
   belongs_to :user
 end
